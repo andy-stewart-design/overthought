d3fc004e1af6dba0e4367d256ee9608517516f43
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2023-05-07 13:16:52 -0700

    trying to remove unnecessary js from image component

--- src/lib/components/media/Image.svelte
+++ src/lib/components/media/Image.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	// TODO: set up for SVGs
-	import { setSrc } from '$lib/utils/sanity';
+	// import { setSrc } from '$lib/utils/sanity';
 	import { setCloudSrc } from '$lib/utils/cloudinary';
 	import { setSizes } from '$lib/utils/images';
 
 	type ImageLoading = 'eager' | 'lazy';
 
 	export let src: string;
-	export let sanity = src.includes('cdn.sanity.io');
+	// export let sanity = src.includes('cdn.sanity.io');
 	export let cloud = false;
 	export let imageWidths: number | number[] = 1200;
 	export let sizes: string | ((string | number)[] | string)[] = '100vw';
@@ -15,7 +15,7 @@
 	export let width = '1080';
 	export let height = '1080';
 	export let loading: ImageLoading = 'eager';
-	export let blur = false;
+	// export let blur = false;
 	export { classname as class };
 
 	let classname: string = '';
@@ -23,7 +23,26 @@
 	if (typeof imageWidths !== 'number' && imageWidths.length) defaultSize = imageWidths.slice(-1)[0];
 </script>
 
-{#if sanity}
+{#if cloud}
+	{#if typeof imageWidths !== 'number'}
+		<img
+			src={setCloudSrc(src, defaultSize)}
+			srcset={setCloudSrc(src, imageWidths)}
+			sizes={setSizes(sizes)}
+			class={classname}
+			{width}
+			{height}
+			{alt}
+			{loading}
+		/>
+	{:else}
+		<img src={setCloudSrc(src, imageWidths)} class={classname} {width} {height} {alt} {loading} />
+	{/if}
+{:else}
+	<img {src} class={classname} {width} {height} {alt} {loading} />
+{/if}
+
+<!-- {#if sanity}
 	{#if typeof imageWidths !== 'number'}
 		<img
 			src={setSrc(src, defaultSize, blur)}
@@ -55,4 +74,4 @@
 	{/if}
 {:else}
 	<img {src} class={classname} {width} {height} {alt} {loading} />
-{/if}
+{/if} -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,39 +7,39 @@
 	import Image from '$lib/components/media/Image.svelte';
 </script>
 
-<!-- <Observer let:observe> -->
-<main class="grid grid-cols-12 border-b bg-background border-light">
-	<!-- <HomeBg /> -->
-	<div class="relative col-span-4">
-		<Container class="sticky top-20 grid gap-y-5 py-12" py="none">
-			<div class="flex gap-5">
-				<Image
-					cloud
-					src="/about/andy-headshot-square.jpg"
-					alt="Headshot"
-					class="h-18 w-18 rounded-full bg-surface-mid"
-				/>
-				<div class="grid gap-1.5">
-					<h2 class="inline-block grow text-4xl font-semibold">Andy Stewart</h2>
-					<h3 class="text-sm font-medium uppercase tracking-widest opacity-70">
-						Design + Technology
-					</h3>
+<Observer let:observe>
+	<main class="grid grid-cols-12 border-b bg-background border-light">
+		<!-- <HomeBg /> -->
+		<div class="relative col-span-4">
+			<Container class="sticky top-20 grid gap-y-5 py-12" py="none">
+				<div class="flex gap-5">
+					<Image
+						cloud
+						src="/about/andy-headshot-square.jpg"
+						alt="Headshot"
+						class="h-18 w-18 rounded-full bg-surface-mid"
+					/>
+					<div class="grid gap-1.5">
+						<h2 class="inline-block grow text-4xl font-semibold">Andy Stewart</h2>
+						<h3 class="text-sm font-medium uppercase tracking-widest opacity-70">
+							Design + Technology
+						</h3>
+					</div>
 				</div>
-			</div>
-			<p>
-				I work at the intersection of brand and technology designing future-focused visual systems —
-				often for screens, sometimes not. I'm currently the Digital Creative Director at Turnstyle,
-				a small-yet-mighty creative studio in Seattle, where I help bring life to brands through
-				strategic, thoughtful design.
-			</p>
-		</Container>
-	</div>
-	<div class="relative col-span-8 min-h-screen border-l border-r border-light">
-		<Container as="section" pxNone pt="none" pb="none" class="">
-			{#each timeline.entries as entry, index}
-				<FeedCard content={entry} {index} />
-			{/each}
-		</Container>
-	</div>
-</main>
-<!-- </Observer> -->
+				<p>
+					I work at the intersection of brand and technology designing future-focused visual systems
+					— often for screens, sometimes not. I'm currently the Digital Creative Director at
+					Turnstyle, a small-yet-mighty creative studio in Seattle, where I help bring life to
+					brands through strategic, thoughtful design.
+				</p>
+			</Container>
+		</div>
+		<div class="relative col-span-8 min-h-screen border-l border-r border-light">
+			<Container as="section" pxNone pt="none" pb="none" class="">
+				{#each timeline.entries as entry, index}
+					<FeedCard content={entry} {observe} {index} />
+				{/each}
+			</Container>
+		</div>
+	</main>
+</Observer>
