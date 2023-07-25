425e4f628307ac5ab46c0ce47b0d3778c8e8fac6
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2023-07-25 15:54:32 +0200

    Footer Updates

--- src/lib/components/global/chrome/Footer.svelte
+++ src/lib/components/global/chrome/Footer.svelte
@@ -31,6 +31,13 @@
 		});
 	};
 
+	const handleClick = () => {
+		window.scrollTo({
+			top: 0,
+			behavior: "smooth",
+		});
+	};
+
 	$: observer = browser && new IntersectionObserver(observerCallback, observerOptions);
 	$: browser && ref && observer && observer.observe(ref);
 
@@ -41,8 +48,39 @@
 	<div class="h-[80vh]" />
 	<div class="sticky bottom-0 h-0">
 		<div class="absolute bottom-0 h-[80vh] w-full bg-surface-mid">
-			<Container class="flex h-full flex-col justify-center" pb="sm">
-				<div class="flex grow items-center justify-center">
+			<Container class="relative flex h-full flex-col pb-md pt-md md:grid md:grid-cols-3 md:pt-xl">
+				<div class="md:flex md:items-end">
+					<button
+						on:click={handleClick}
+						class="flex h-32 w-32 items-center justify-center rounded-full border transition-colors duration-300 ease-out-3 border-light can-hover:hover:bg-foreground can-hover:hover:text-background"
+					>
+						<svg width="80" height="80" viewBox="0 0 80 80" fill="none">
+							<path d="M40 16V64" stroke="currentColor" fill="none" stroke-width="6" />
+							<path d="M16 40L40 16L64 40" stroke="currentColor" fill="none" stroke-width="6" />
+						</svg>
+					</button>
+				</div>
+				<div class="md:pt-none flex grow flex-col justify-between pt-sm md:col-span-2">
+					<div class="space-y-12">
+						<p class="max-w-3xl text-4xl leading-snug text-foreground md:text-5xl md:leading-[1.2]">
+							Thank you for scrolling! <span class="text-foreground/60">Want to chat?</span>
+							<a href="mailto:andy.stewart1170@gmail.com" class="transition-opacity hover:opacity-60">Reach out</a>.
+							<span class="text-foreground/60">Tired of being at the bottom?</span>
+							<button on:click={handleClick} class="transition-opacity hover:opacity-60">Head back up</button>.
+						</p>
+						<div class="space-y-4">
+							<p class="text-sm font-medium opacity-65">Follow Me</p>
+							<div class="space-x-3 text-lg font-medium">
+								<a href="https://codepen.io/andyandyandyoh">Codepen</a>
+								<a href="https://github.com/andy-stewart-design">Github</a>
+								<a href="https://twitter.com/spinbutton">Twitter</a>
+								<a href="https://www.instagram.com/andystewartdesign/">Instagram</a>
+							</div>
+						</div>
+					</div>
+					<p class="text-sm font-medium">© Andy Stewart Design {year}</p>
+				</div>
+				<!-- <div class="flex grow items-center justify-center">
 					<button
 						on:click={() => {
 							window.scrollTo({
@@ -73,7 +111,7 @@
 				<div class="flex justify-between pb-3">
 					<p>©{year}</p>
 					<a href="mailto:andy.stewart1170@gmail.com">Contact</a>
-				</div>
+				</div> -->
 			</Container>
 		</div>
 	</div>
