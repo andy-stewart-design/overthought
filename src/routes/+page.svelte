fbb434149f715b9df6961fe8c8bd061714779b65
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2023-08-06 13:30:58 +0200

    adding recommendations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,8 +10,11 @@
 	<Container class="mx-auto w-full max-w-app">
 		<section id="home" class="grid gap-x-8 border-b py-lg border-light md:grid-cols-2 md:gap-y-12 md:py-xl">
 			<div class="col-span-full mb-8 md:mb-0">
-				<div class="inline-block rounded-full border py-1 pl-2.5 pr-3 border-light">
-					<span class="mr-2 inline-block h-3 w-3 animate-pulse rounded-full bg-green-500" />
+				<div class="inline-flex items-center gap-2 rounded-full border py-1 pl-2.5 pr-3 border-light">
+					<span class="relative flex h-3 w-3">
+						<span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75" />
+						<span class="relative inline-flex h-3 w-3 rounded-full bg-green-500" />
+					</span>
 					Open for work
 				</div>
 			</div>
@@ -22,16 +25,19 @@
 			<div />
 			<div class="space-y-6">
 				<p class="text-base text-foreground/80 xs:text-lg md:text-base lg:text-lg">
-					Hi, I’m Andy. I’m a product designer helping to bring life to brands through compelling visual systems and
-					clear interfaces.
+					Hi, I’m Andy. I’m a product designer with over a decade of experience helping to bring life to brands through
+					compelling visual systems and clear interfaces.
 				</p>
 				<p class="text-base text-foreground/80 xs:text-lg md:text-base lg:text-lg">
 					I was most recently the Digital Creative Director at <a
 						class="font-semibold text-foreground"
 						href="https://turnstyle.studio/"
 						target="_blank">Turnstyle</a
-					>, a full-service creative studio in Seattle, where I worked end-to-end on projects both as a high-level
-					creative leader and a hands-on contributor (often at the same time).
+					>, a full-service creative studio in Seattle, where I worked end-to-end on projects as both a high-level
+					creative leader and a hands-on contributor (often at the same time). I also taught design at the
+					<a class="font-semibold text-foreground" href="https://www.svcseattle.com/" target="_blank"
+						>School of Visual Concepts</a
+					>.
 				</p>
 				<p class="text-base text-foreground/80 xs:text-lg md:text-base lg:text-lg">
 					I'm currently available for hire. If you would like to discuss opportunities to collaborate or the types of
@@ -58,8 +64,8 @@
 			id="work"
 			class="grid gap-2 gap-y-12 border-b py-lg border-light sm:grid-cols-2 sm:gap-x-8 md:py-xl lg:grid-cols-3"
 		>
-			<h1 class="col-span-full text-5xl font-medium xs:text-6xl md:text-5xl lg:text-6xl">Projects</h1>
-			{#each data.projects as project}
+			<h2 class="col-span-full text-5xl font-medium xs:text-6xl md:text-5xl lg:text-6xl">Projects</h2>
+			{#each data.projects as project, index}
 				<div class="space-y-2">
 					<div class="space-y-3 pt-4">
 						{#if project.metadata.link?.external}
@@ -71,6 +77,7 @@
 									alt={project.metadata.img.alt}
 									imageWidths={[800, 1200]}
 									sizes={[["769px", "50vh"], ["100vh"]]}
+									loading={index >= 3 ? "lazy" : "eager"}
 								/>
 								<div class="flex items-baseline justify-between">
 									<p class="text-2xl font-medium">{project.metadata.client} <span class="opacity-65">↗</span></p>
@@ -107,6 +114,34 @@
 			{/each}
 		</section>
 
+		<section
+			id="work"
+			class="mx-auto grid max-w-6xl gap-3 border-b py-lg border-light sm:grid-cols-2 md:gap-6 md:py-xl"
+		>
+			<h2 class="col-span-full mb-8 text-center text-3xl font-medium md:mb-4">Kind words from some kind people</h2>
+
+			{#each data.recos as reco}
+				<div class="space-y-4 rounded-xl border bg-foreground/10 p-4 border-light">
+					<div class="flex items-center gap-4">
+						<Image
+							cloud
+							class="h-14 w-14 shrink-0 rounded-full border object-cover border-light"
+							src={reco.avatar}
+							alt={`Headshot of ${reco.name}`}
+							imageWidths={240}
+							sizes={[["56px"]]}
+							loading="lazy"
+						/>
+						<div>
+							<p class="pt-px text-lg font-medium">{reco.name}</p>
+							<p class="text-sm font-medium opacity-65">{reco.title}, {reco.company}</p>
+						</div>
+					</div>
+					<p>{reco.quote}</p>
+				</div>
+			{/each}
+		</section>
+
 		<section id="about" class="grid gap-8 py-lg md:grid-cols-2 md:gap-y-12 md:py-xl">
 			<div>
 				<Image
@@ -120,21 +155,14 @@
 			<div class="flex flex-col justify-center gap-6">
 				<h1 class="text-5xl font-medium xs:text-6xl md:text-5xl lg:text-6xl">About Me</h1>
 				<p class="text-base text-foreground/80 xs:text-lg md:text-base lg:text-lg">
-					I've been practicing design for over a decade, both as a design leader and a hands-on contributor — often at
-					the same time. Most recently, I was Digital Creative Director at <a
-						class="font-semibold text-foreground"
-						href="https://turnstyle.studio/"
-						target="_blank">Turnstyle</a
-					>, a full-service creative agency in Seattle, as well as a instructor at the
-					<a class="font-semibold text-foreground" href="https://www.svcseattle.com/" target="_blank"
-						>School of Visual Concepts</a
-					>.
+					I'm a designer with a passion for bringing ideas to life, by any means necessary. In my work, I enjoy finding
+					opportunities for creativity amidst the constraints of a given project and chasing ideas that push me to the
+					edge of my abilities. I’m a designer first and foremost, but I’m also a self-taught developer with a
+					background in journalism, and I’m passionate about helping to bridge the gaps between disciplines.
 				</p>
 				<p class="text-base text-foreground/80 xs:text-lg md:text-base lg:text-lg">
-					In my work, I enjoy finding opportunities for creativity amidst the constraints of a given project and chasing
-					ideas that push me to the edge of my abilities. I’m a designer first and foremost, but I’m also a self-taught
-					developer with a background in journalism, and I’m passionate about helping to bridge the gap between
-					disciplines.
+					When I'm not behind a computer, you may find me at a concert, on a run, making noises on a synthesizer, or
+					mixing cocktails.
 				</p>
 				<p class="text-base text-foreground/80 xs:text-lg md:text-base lg:text-lg">
 					Though I've spent the last 10 years in Seattle, I'm currently traveling and will soon be moving to New York
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,11 +1,14 @@
 import type { PageLoad } from "./$types";
 import projectData from "@content/projects.json";
+import recoData from "@content/recommendations.json";
 
 export const load: PageLoad = () => {
 	const projects = projectData.entries;
+	const recos = recoData.entries;
 
 	return {
 		projects,
+		recos,
 		meta: {
 			title: "Andy Stewart | Design + Code",
 			description:
