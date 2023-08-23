5c9a8217f721745031d7d1b2fe2ff605de7c5d00
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2023-08-22 20:45:10 -0400

    Copy tweak

--- src/routes/work/get-some-ipsum/+page.svelte
+++ src/routes/work/get-some-ipsum/+page.svelte
@@ -183,8 +183,8 @@
 		</div>
 
 		<p>
-			Out of respect for users needs and preferences, the plugin is fully keyboard accessible, including focus states,
-			and features built-in support for both dark and light mode.
+			To respect and respond to users needs and preferences, the plugin is fully keyboard accessible, including focus
+			states, and features built-in support for both dark and light mode.
 		</p>
 
 		<div class="mx-auto w-full max-w-6xl space-y-4 pb-4 pt-6">
@@ -201,7 +201,7 @@
 		</div>
 
 		<p>
-			This is my first Figma plugin, which I built from the ground up using Svelte as my front end framework for
+			This was my first Figma plugin, which I built from the ground up using Svelte as my front end framework for
 			templating and state management and Tailwind as my CSS framework for managing styles. It's real, and you can check
 			it out <a href="https://www.figma.com/community/plugin/1225446981019698507/Get-Some-Ipsum"
 				>here in the Figma community</a
